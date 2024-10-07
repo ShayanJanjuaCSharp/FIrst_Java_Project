eb725d867e29f14ab8adbbcec35455b6b940b76a
Shayan Janjua <shayanjanjua.london@gmail.com>
2024-10-07 10:12:32 +0100

    first commit

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String[] names = {"shayan", "Adrian" };
+        for(String s : names){
+            System.out.println(s);
+        }
+    }
+}
